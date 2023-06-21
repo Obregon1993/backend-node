00b39641ddbe3558933172f1bad6fc7c3e3182a8
Carlos Obregon <obregonlara1993@gmail.com>
2023-06-21 00:11:19 -0400

    adding middleware to authenticate user

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const secure = require("./secure");
 const response = require("../../../network/response");
 const Controller = require("./index.js");
 
@@ -9,7 +9,7 @@ const router = express.Router();
 router.get("/", list);
 router.get("/:id", get);
 router.post("/", upsert);
-router.put("/", upsert);
+router.put("/", secure("update"), upsert);
 
 function list(req, res) {
   Controller.list()
--- auth/index.js
+++ auth/index.js
@@ -1,9 +1,46 @@
 const jwt = require("jsonwebtoken");
+const config = require("../config");
+const secret = config.jwt.secret;
 
 function sign(data) {
-  return jwt.sign(data, "secret");
+  return jwt.sign(data, secret);
+}
+
+function verify(token) {
+  return jwt.verify(token, secret);
+}
+
+const check = {
+  own: function (req, owner) {
+    const decoded = decodeHeader(req);
+    console.log(decoded, owner);
+    if (decoded.id !== owner) {
+      throw new Error("Permission denied");
+    }
+  },
+};
+
+function getToken(auth) {
+  if (!auth) {
+    throw new Error("No token provided");
+  }
+  if (auth.indexOf("Bearer") === -1) {
+    throw new Error("Not valid token format");
+  }
+  let token = auth.replace("Bearer", "");
+  return token;
+}
+
+function decodeHeader(req) {
+  const authorization = req.headers.authorization || "";
+  const token = getToken(authorization);
+  const decoded = verify(token);
+
+  req.user = decoded;
+  return decoded;
 }
 
 module.exports = {
   sign,
+  check,
 };
